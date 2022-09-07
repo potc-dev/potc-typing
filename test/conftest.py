146826ad670f9f97258351efeacdcb92231e017d
HansBug <hansbug@buaa.edu.cn>
2022-09-07 17:49:02 +0800

    dev(hansbug): use clear command

--- test/conftest.py
+++ test/conftest.py
@@ -5,5 +5,5 @@ from potc.testing import mock_potc_plugins
 
 @pytest.fixture(autouse=True)
 def potc_typing_installed():
-    with mock_potc_plugins('potc_typing.plugin'):
+    with mock_potc_plugins('potc_typing.plugin', clear=True):
         yield
